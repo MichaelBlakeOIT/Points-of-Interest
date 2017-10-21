3051b1a5bc6520c9b778bbc80c053b5e2e1f29b8
MichaelBlakeOIT <michael.blake@oit.edu>
2017-10-20 19:14:33 -0700

    restructured file system

---
+++ client/PointsofInterest/app/src/main/java/poi/michael/pointsofinterest/LoginActivity.java
@@ -0,0 +1,258 @@
+package poi.michael.pointsofinterest;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    /**
+     * Id to identity READ_CONTACTS permission request.
+     */
+    private static final int REQUEST_READ_CONTACTS = 0;
+
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+    private UserLoginTask mAuthTask = null;
+
+    // UI references.
+    private AutoCompleteTextView mEmailView;
+    private EditText mPasswordView;
+    private View mProgressView;
+    private View mLoginFormView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_login);
+        // Set up the login form.
+        //mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+        /*populateAutoComplete();
+
+        mPasswordView = (EditText) findViewById(R.id.password);
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });*/
+
+        Button mSignInButton = (Button) findViewById(R.id.sign_in_button);
+        TextView RegisterTextView = (TextView) findViewById(R.id.registerSelect);
+
+        mSignInButton.setOnClickListener(mSignInButtonListener);
+        RegisterTextView.setOnClickListener(mRegisterSelectListener);
+
+        /*mEmailSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });*/
+
+       // mLoginFormView = findViewById(R.id.login_form);
+        //mProgressView = findViewById(R.id.login_progress);
+    }
+
+    private View.OnClickListener mSignInButtonListener = new View.OnClickListener()
+    {
+        public void onClick(View v)
+        {
+            AutoCompleteTextView username = (AutoCompleteTextView) findViewById(R.id.UsernameLoginField);
+            EditText password = (EditText) findViewById(R.id.passwordLoginField);
+
+            new UserLoginTask(username.getText().toString(), password.getText().toString()).execute();
+            //mSignInButton.setText("TEST");
+            // Instantiate the RequestQueue.
+
+        }
+    };
+
+    private View.OnClickListener mRegisterSelectListener = new View.OnClickListener()
+    {
+
+        @Override
+        public void onClick(View v) {
+            Intent RegisterActivityIntent = new Intent(LoginActivity.this, activity_signup.class);
+            LoginActivity.this.startActivity(RegisterActivityIntent);
+        }
+    };
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void attemptLogin() {
+        if (mAuthTask != null) {
+            return;
+        }
+
+        // Reset errors.
+        mEmailView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            mEmailView.setError(getString(R.string.error_invalid_email));
+            focusView = mEmailView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            mAuthTask = new UserLoginTask(email, password);
+            mAuthTask.execute((Void) null);
+        }
+    }
+
+    private boolean isEmailValid(String email) {
+        //TODO: Replace this with your own logic
+        return email.contains("@");
+    }
+
+    private boolean isPasswordValid(String password) {
+        //TODO: Replace this with your own logic
+        return password.length() > 4;
+    }
+
+
+    /**
+     * Represents an asynchronous login/registration task used to authenticate
+     * the user.
+     */
+    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String mEmail;
+        private final String mPassword;
+        private Context mContext;
+
+        UserLoginTask(String email, String password) {
+            mEmail = email;
+            mPassword = password;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // TODO: attempt authentication against a network service.
+            String url = "http://192.168.1.14:80/session";
+            mContext = getApplicationContext();
+
+            //Button mSignInButton = (Button) findViewById(R.id.sign_in_button);
+
+            StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                    new Response.Listener<String>()
+                    {
+                        @Override
+                        public void onResponse(String response) {
+                            // response
+                            try {
+                                JSONObject JSONResponse = new JSONObject(response);
+                                if (JSONResponse.getBoolean("success") == true) {
+                                    Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(getApplicationContext(), "Incorrect login", Toast.LENGTH_SHORT).show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                            //Log.d("Response", response);
+                        }
+                    },
+                    new Response.ErrorListener()
+                    {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            // error
+                            //Log.d("Error.Response", error.toString());
+                        }
+                    }
+            ) {
+                @Override
+                protected Map<String, String> getParams()
+                {
+                    Map<String, String>  params = new HashMap<String, String>();
+                    params.put("username", mEmail);
+                    params.put("password", mPassword);
+
+                    return params;
+                }
+            };
+            volleySingleton.getInstance(mContext).getRequestQueue().add(postRequest);
+            // TODO: register the new account here.
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mAuthTask = null;
+            //showProgress(false);
+
+            if (success) {
+                //finish();
+            } else {
+                //mPasswordView.setError(getString(R.string.error_incorrect_password));
+                //mPasswordView.requestFocus();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mAuthTask = null;
+            //showProgress(false);
+        }
+    }
+}
+
---
+++ client/PointsofInterest/app/src/main/java/poi/michael/pointsofinterest/volleySingleton.java
@@ -0,0 +1,39 @@
+package poi.michael.pointsofinterest;
+
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by michael1026 on 10/19/2017.
+ */
+
+class volleySingleton {
+    private static volleySingleton mInstance;
+    private RequestQueue mQueue;
+    private static Context mContext;
+
+    static volleySingleton getInstance(Context context) {
+        if(mInstance == null){
+            mInstance = new volleySingleton(context);
+        }
+        // Return MySingleton new Instance
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue(){
+        // If RequestQueue is null the initialize new RequestQueue
+        if(mQueue == null){
+            mQueue = Volley.newRequestQueue(mContext.getApplicationContext());
+        }
+
+        // Return RequestQueue
+        return mQueue;
+    }
+
+    private volleySingleton(Context context) {
+        mContext = context;
+        mQueue = getRequestQueue();
+    }
+}
