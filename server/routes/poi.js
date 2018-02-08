b51ed2203ff49e921f0b5177370372ad366d2b8a
MichaelBlakeOIT <michael.blake@oit.edu>
2018-02-07 22:37:16 -0800

    fixed some bugs

--- server/routes/poi.js
+++ server/routes/poi.js
@@ -14,7 +14,8 @@ router.post('/', requireAuth,
         field("title").required().minLength(1).maxLength(45),
         field("description").maxLength(600)
     ), function(req, res) {
-
+        if (!req.form.isValid)
+            return res.json({ success: false, message: req.form.errors });
         //format insert
         var insert = "INSERT INTO point_of_interests (coordinates, user_id, title, description) VALUES " +
                      "(ST_GeomFromText('POINT(" + req.body.lat + " " + req.body.long + ")'), " + req.user.user_id +
@@ -46,21 +47,16 @@ router.get('/', requireAuth, function(req, res) {
 
 });
 
-router.get('/:id', requireAuth, 
-    form(
-        field("req.params.id").isNumeric()
-    ),
-    function(req, res) {
-        var getPOI = "SELECT point_of_interests.user_id, pio_id, ST_X(coordinates) AS \"lat\", ST_Y(coordinates) AS \"long\", title, description FROM point_of_interests, users WHERE pio_id = " + req.params.id + " AND point_of_interests.user_id = users.user_id;";
+router.get('/:id', requireAuth, function(req, res) {
+    var getPOI = "SELECT point_of_interests.user_id, pio_id, ST_X(coordinates) AS \"lat\", ST_Y(coordinates) AS \"long\", title, description FROM point_of_interests, users WHERE pio_id = " + config.pool.escape(req.params.id) + " AND point_of_interests.user_id = users.user_id;";
 
-        config.pool.query(getPOI, function(err, rows) {
-            if(err) {
-                console.log(err);
-                return res.json({ success: false, message: "Unknown error" });
-            }
-            res.json({ success: true, data: rows[0] });
-        });
-
+    config.pool.query(getPOI, function(err, rows) {
+        if(err) {
+            console.log(err);
+            return res.json({ success: false, message: "Unknown error" });
+        }
+        res.json({ success: true, data: rows[0] });
+    });
 });
 
 module.exports = router;
